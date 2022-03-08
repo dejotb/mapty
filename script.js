bacccc63464ba266a55a8c23e9cb218a7e0df303
dejotb <dejotb@gmail.com>
2022-03-08 18:50:23 +0100

    added geolocation API

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // prettier-ignore
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
@@ -10,3 +8,22 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+const map = document.querySelector('#map');
+
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+
+      console.log(`https://www.google.pl/maps/@${latitude},${longitude}`);
+      map.innerHTML = `
+      <a href="https://www.google.pl/maps/@${latitude},${longitude}" target="_blank">Click Here
+      </a>
+      `;
+    },
+
+    function () {
+      alert('Could not get your position');
+    }
+  );
